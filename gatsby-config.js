fccc0693370cfa256d78f61fde73404960a03b8e
Smai Fullerton <shfullerton@gmail.com>
2020-01-13 17:19:05 -0700

    I do not know what this is, attempts from weeks ago to change the underlying map - dropping for now

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,38 +1,52 @@
+const googleAnalyticsId = `UA-XXXXX`
+
 module.exports = {
   siteMetadata: {
+    siteUrl: `https://mt-nonprofit-data.netlify.com/`,
     title: `Montana Nonprofits`,
     description: `A map of nonprofits for you to filter in interesting ways.`,
     author: `@code-for-montana`,
+    googleAnalyticsId,
+    mapboxToken: process.env.GATSBY_MAPBOX_API_TOKEN,
   },
   plugins: [
-    {
-      resolve: 'gatsby-plugin-react-leaflet',
-      options: {
-        linkStyles: true // (default: true) Enable/disable loading stylesheets via CDN
-      },
-    },
     `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: `gatsby-plugin-styled-components`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        displayName: process.env.NODE_ENV !== `production`,
+        fileName: false,
       },
     },
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `./config/typography.js`,
+      },
+    },
+    `gatsby-plugin-catch-links`,
+    `gatsby-plugin-sitemap`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: googleAnalyticsId,
+        anonymize: true,
+      },
+    },
+    // {
+    //   resolve: `gatsby-plugin-manifest`,
+    //   options: {
+    //     name: `gatsby-starter-default`,
+    //     short_name: `starter`,
+    //     start_url: `/`,
+    //     background_color: `#663399`,
+    //     theme_color: `#663399`,
+    //     display: `minimal-ui`,
+    //     // icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+    //   },
+    // },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
--- gatsby-node.js
+++ gatsby-node.js
@@ -1,35 +1,23 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
-
-// You can delete this file if you're not using it
-
-const fs = require("fs")
 const path = require("path")
-const slugify = require("slugify")
 
-exports.createPages = ({ graphql, actions }) => {
-  const { createPage } = actions
-
-  const modules = fs.readdirSync("modules", { withFileTypes: true }).filter(dir => dir.isDirectory()).map(dir => dir.name)
-
-  for (const module of modules) {
-    const module_path = path.join("modules", module)
-
-    const config_path = path.join(module_path, "config.json")
-    const config_text = fs.readFileSync(config_path).toString()
-    const config = JSON.parse(config_text)
-
-    const component_path = path.join(module_path, "content.js")
-
-    createPage({
-      path: `/${slugify(config.name, '-')}`,
-      component: path.resolve(component_path),
-      context: {
-        name: config.name,
-      },
-    });
+exports.onCreateWebpackConfig = ({ actions, stage, loaders }) => {
+  const config = {
+    resolve: {
+      modules: [path.resolve(__dirname, "src"), "node_modules"],
+    },
   }
+
+  // when building HTML, window is not defined, so Leaflet causes the build to blow up
+  if (stage === "build-html") {
+    config.module = {
+      rules: [
+        {
+          test: /mapbox-gl/,
+          use: loaders.null(),
+        },
+      ],
+    }
+  }
+
+  actions.setWebpackConfig(config)
 }
