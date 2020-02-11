5f6513d583d095fc5d57e201e4758b6ddadda44c
Larry <50931759+Turkinolith@users.noreply.github.com>
2020-02-10 17:50:42 -0700

    Removed attribution footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -10,7 +10,6 @@ import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
-import Footer from "./footer"
 import "./layout.css"
 
 const Layout = ({ children }) => {
@@ -37,7 +36,6 @@ const Layout = ({ children }) => {
       >
         <main>{children}</main>
       </div>
-      <Footer />
     </>
   )
 }
